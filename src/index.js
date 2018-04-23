c4f66bb36d8ae6a70099fc4583987fe797a222ee
Victor Hugo <hugo.hsn@gmail.com>
2018-04-22 20:32:23 -0400

    Added Mutation

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ const movies = require('./data/movies');
 const studios = require('./data/studios');
 const casts = require('./data/cast');
 
+
+let movieIndex = movies.length;
+
 const resolvers = {
   Query: {
     allMovies: (root, args, context) => {
       return movies;
     },
   },
+  Mutation: {
+    createMovie: (root, args, context) => {
+      const newMovie = {
+        id: `movie${movieIndex++}`,
+        ...args,
+      };
 
+      movies.push(newMovie);
+
+      return newMovie;
+    },
+  },
   Movie: {
     studio: (root, args, context) => {
       return studios.find(studio => {
@@ -20,9 +34,9 @@ const resolvers = {
       return casts.find(cast => {
         return cast.moviesId.find(movieId => movieId === root.id);
       });
- 
+      
     }
-  }, 
+  },
 };
 
 const server = new GraphQLServer({
